bc0221002dc1a16191572149d2155c75f7c48520
Omar Ahmed <73648173+Omar2454@users.noreply.github.com>
2024-01-26 19:57:26 +0200

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MYHEADER_H
+#define MYHEADER_H
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
